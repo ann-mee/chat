5a0a3c54e18f3c8b4b3874a725bfd9bc86da3acd
Ann <an-mo@wp.pl>
2021-01-18 11:54:42 +0100

    Added routes for login and register

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'
+
+class Register extends React.Component {
+    render(){
+        return (
+            <div>Register</div>
+        )
+    }
+}
+
+export default Register
